37d0aa493534bea7e6f4a09c33d023acdde8d991
GrBaMzEhD <gbryant801@gmail.com>
2023-10-02 16:39:41 +0100

    Added SIGTERM config

--- server.js
+++ server.js
@@ -35,3 +35,10 @@ process.on('unhandledRejection', (err) => {
         process.exit(1);
     });
 });
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM RECEIVED, shutting down server.');
+    server.close(() => {
+        console.log('Process terminated.')
+    })
+})
